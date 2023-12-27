2a7ef1e437a59d1bf6db8128f8e8ba914b540778
hyeon9782 <hyeon9782@gmail.com>
2023-12-27 16:46:39 +0900

    📝 docs: package.json / vite.config.ts 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vanillaExtractPlugin(), react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/components/index.ts"),
+      name: "@black-ui/react",
+      fileName: "black-ui",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
 });
