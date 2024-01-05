23e58dd3bc4d2eddbaabd6f53a96a0c1f91051a4
hyeon9782 <hyeon9782@gmail.com>
2024-01-05 22:14:04 +0900

    ✨ feat: Toast 컴포넌트 관련 파일 생성

---
+++ src/components/Toast/Toast.css.ts
@@ -0,0 +1,7 @@
+import { recipe } from "@vanilla-extract/recipes";
+
+export const toast = recipe({
+  base: {},
+  variants: {},
+  compoundVariants: [],
+});
---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,7 @@
+import { toast } from "./Toast.css";
+
+const Toast = () => {
+  return <div className={toast({})}>Toast</div>;
+};
+
+export default Toast;
