b0d48d59f2dbe47de4ceb9e00d8d0b49cec4261d
hyeon9782 <hyeon9782@gmail.com>
2024-01-07 00:19:42 +0900

    ✨ feat: List 관련 파일 생성

---
+++ src/components/List/List.css.ts
@@ -0,0 +1,7 @@
+import { recipe } from "@vanilla-extract/recipes";
+
+export const list = recipe({
+  base: {},
+  variants: {},
+  compoundVariants: [],
+});
---
+++ src/components/List/List.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { list } from "./List.css";
+
+const List = () => {
+  return <div className={list({})}>List</div>;
+};
+
+export default List;
