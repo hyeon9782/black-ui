d90eebb7663c1b081f7cfb85046ed3fb1f4e4415
hyeon9782 <hyeon9782@gmail.com>
2024-01-08 01:59:55 +0900

    ✨ feat: Skeleton 관련 파일 생성

---
+++ src/components/Skeleton/Skeleton.css.ts
@@ -0,0 +1,7 @@
+import { recipe } from "@vanilla-extract/recipes";
+
+export const skeleton = recipe({
+  base: {},
+  variants: {},
+  compoundVariants: [],
+});
---
+++ src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Skeleton = () => {
+  return <div>Skeleton</div>;
+};
+
+export default Skeleton;
