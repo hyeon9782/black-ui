a3b41bd37f9b9c7360f22998c1ccaefa4a0a28cc
hyeon9782 <hyeon9782@gmail.com>
2024-01-09 18:45:10 +0900

    ✨ feat: Accordion 관련 파일 생성

---
+++ src/components/Accordion/Accordion.css.ts
@@ -0,0 +1,7 @@
+import { recipe } from "@vanilla-extract/recipes";
+
+export const accordion = recipe({
+  base: {},
+  variants: {},
+  compoundVariants: [],
+});
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { accordion } from "./Accordion.css";
+
+const Accordion = () => {
+  return <div className={accordion({})}>Accordion</div>;
+};
+
+export default Accordion;
