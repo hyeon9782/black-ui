b0430f552c32b8a11718d8c078e44275a1d39750
hyeon9782 <hyeon9782@gmail.com>
2024-01-10 13:58:58 +0900

    ✨ feat: Popover 컴포넌트 관련 파일 생성

---
+++ src/components/Popover/Popover.css.ts
@@ -0,0 +1,7 @@
+import { recipe } from "@vanilla-extract/recipes";
+
+export const popover = recipe({
+  base: {},
+  variants: {},
+  compoundVariants: [],
+});
---
+++ src/components/Popover/Popover.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { popover } from "./Popover.css";
+
+const Popover = () => {
+  return <div className={popover({})}>Popover</div>;
+};
+
+export default Popover;
