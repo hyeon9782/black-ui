bab01075e059be76f1414bc303df1f2a9e3d7f5d
hyeon9782 <hyeon9782@gmail.com>
2024-01-17 12:16:32 +0900

    ✨ feat: ButtomSheets, SideSheets 관련 파일 생성

---
+++ src/components/Overlay/BottomSheets/BottomSheets.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const BottomSheets = () => {
+  return <div>BottomSheets</div>;
+};
---
+++ src/components/Overlay/SideSheets/SideSheets.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SideSheets = () => {
+  return <div>SideSheets</div>;
+};
+
+export default SideSheets;
