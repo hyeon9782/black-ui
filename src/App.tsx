23bcc33b9436116938778e41fc903899e0f98af3
hyeon9782 <hyeon9782@gmail.com>
2024-01-17 12:53:17 +0900

    ✨ feat: useDisclosure 커스텀 훅 개발

--- src/App.tsx
+++ src/App.tsx
@@ -10,32 +10,35 @@ import FormArea from "./components/FormArea";
 import OverlayArea from "./components/OverlayArea";
 import FeedbackArea from "./components/FeedbackArea";
 import DataDisplayArea from "./components/DataDisplayArea";
+
 import React from "react";
 
 function App() {
   return (
-    <Tabs>
-      <TabList>
-        <Tab>Form</Tab>
-        <Tab>Overlay</Tab>
-        <Tab>Feedback</Tab>
-        <Tab>DataDisplay</Tab>
-      </TabList>
-      <TabPanels>
-        <TabPanel>
-          <FormArea />
-        </TabPanel>
-        <TabPanel>
-          <OverlayArea />
-        </TabPanel>
-        <TabPanel>
-          <FeedbackArea />
-        </TabPanel>
-        <TabPanel>
-          <DataDisplayArea />
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+    <>
+      <Tabs>
+        <TabList>
+          <Tab>Form</Tab>
+          <Tab>Overlay</Tab>
+          <Tab>Feedback</Tab>
+          <Tab>DataDisplay</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <FormArea />
+          </TabPanel>
+          <TabPanel>
+            <OverlayArea />
+          </TabPanel>
+          <TabPanel>
+            <FeedbackArea />
+          </TabPanel>
+          <TabPanel>
+            <DataDisplayArea />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
   );
 }
 
