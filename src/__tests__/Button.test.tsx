a97eac8c7553e00e0e73782c78cf559d45a1de87
hyeon9782 <hyeon9782@gmail.com>
2024-01-26 19:29:30 +0900

    🐛 fix: Button 테스트 코드 수정

--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -1,13 +1,14 @@
 // Button.test.tsx
+import React from "react";
 import "@testing-library/jest-dom";
 import { cleanup, fireEvent, render, screen } from "@testing-library/react";
-import Button from "../components/Button";
+import { Button } from "../components";
 
 describe("Button", () => {
   afterEach(() => cleanup());
 
   it("renders", () => {
-    render(<Button data-testid="btn" />);
+    render(<Button data-testid="btn">Button</Button>);
     const button = screen.getByTestId("btn");
 
     expect(button).toBeInTheDocument();
@@ -15,7 +16,11 @@ describe("Button", () => {
 
   it("fires an event on onClick", () => {
     const fn = jest.fn();
-    render(<Button onClick={fn} data-testid="btn" />);
+    render(
+      <Button onClick={fn} data-testid="btn">
+        Button
+      </Button>
+    );
 
     const button = screen.getByTestId("btn");
     fireEvent.click(button);
@@ -24,7 +29,11 @@ describe("Button", () => {
 
   it("renders the correct classes", () => {
     const fn = jest.fn();
-    render(<Button size="lg" onClick={fn} data-testid="p-btn" />);
+    render(
+      <Button size="lg" onClick={fn} data-testid="p-btn">
+        Button
+      </Button>
+    );
 
     const pBtn = screen.getByTestId("p-btn");
     expect(pBtn).toHaveClass("hyeon-button");
