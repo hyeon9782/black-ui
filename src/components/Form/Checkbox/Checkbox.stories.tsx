e6a4d5ebfcba40c10f19034089e9869b84db0b13
hyeon9782 <hyeon9782@gmail.com>
2024-01-28 00:36:24 +0900

    ✨ feat: Checkbox 컴포넌트 storybook 코드 작성

--- src/components/Form/Checkbox/Checkbox.stories.tsx
+++ src/components/Form/Checkbox/Checkbox.stories.tsx
@@ -1,32 +1,32 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Button from "./Button";
+import Checkbox from "./Checkbox";
 
 const meta = {
-  title: "Component/Form/Button",
-  component: Button,
+  title: "Component/Form/Checkbox",
+  component: Checkbox,
   tags: ["autodocs"],
   parameters: { layout: "centered" },
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof Checkbox>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Small: Story = {
-  args: { size: "sm", children: "Click Me" },
+  args: { size: "sm", children: "Check Me" },
 };
 
 export const Medium: Story = {
-  args: { size: "md", children: "Click Me" },
+  args: { size: "md", children: "Check Me" },
 };
 
 export const LargeOnClick: Story = {
   args: {
     size: "lg",
-    children: "Click Me",
+    children: "Check Me",
     onClick: () => alert("Clicked!"),
   },
 };
 
 export const Disabled: Story = {
-  args: { size: "lg", children: "Click Me", disabled: true },
+  args: { size: "lg", children: "Check Me", disabled: true },
 };
--- src/components/Form/Input/Input.stories.ts
+++ src/components/Form/Input/Input.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import Input from "./Input";
 
 const meta = {
-  title: "UI/Input",
+  title: "Component/Form/Input",
   component: Input,
   tags: ["autodocs"],
   parameters: { layout: "centered" },
