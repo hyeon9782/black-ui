e07f727202bf3311a779a81e89bcf24ef647a53f
hyeon9782 <hyeon9782@gmail.com>
2024-01-29 11:33:31 +0900

    ✨ feat: Menu 컴포넌트 storybook 코드 작성

---
+++ src/components/Overlay/Menu/Menu.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Menu from "./Menu";
+import MenuButton from "./MenuButton";
+import MenuItem from "./MenuItem";
+import MenuList from "./MenuList";
+
+const meta = {
+  title: "Component/Overlay/Menu",
+  component: Menu,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+    componentSubtitle: "Menu 컴포넌트입니다.",
+    docs: {
+      description: {
+        component: "숨겨진 Menu 컴포넌트를 구현할 때 사용합니다.",
+      },
+    },
+  },
+} satisfies Meta<typeof Menu>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Left: Story = {
+  args: {
+    children: <></>,
+  },
+  render: function Render(args) {
+    return (
+      <>
+        <Menu>
+          <MenuButton>Menu 나와라!</MenuButton>
+          <MenuList>
+            <MenuItem
+              onClick={() => {
+                alert("다운로드!");
+              }}
+            >
+              Download
+            </MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
+      </>
+    );
+  },
+};
