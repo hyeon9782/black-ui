4e7eefaaefd90dcae44649a0cbb7889a07339aaa
hyeon9782 <hyeon9782@gmail.com>
2024-02-01 13:12:26 +0900

    ✅ test: Button 컴포넌트 테스트 코드 작성
    1. Button 컴포넌트가 정상적으로 렌더링된다.
    2. onClick 이벤트가 정상적으로 트리거된다.
    3. icons와 함께 렌더링된다.
    4. loadingText가 렌더링된다.
    5. Spinner가 렌더링된다.
    6. 비활성화된다.
    7. aria 속성이 정상적으로 적용 된다.

--- src/components/Form/Button/Button.test.tsx
+++ src/components/Form/Button/Button.test.tsx
@@ -1,20 +1,20 @@
 // Button.test.tsx
 
-import "@testing-library/jest-dom";
 import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import Button from "./Button";
+import { IoMdClose } from "react-icons/io";
 
-describe("Button", () => {
+describe("Button 컴포넌트 테스트", () => {
   afterEach(() => cleanup());
 
-  it("renders", () => {
+  it("Button 컴포넌트가 정상적으로 렌더링된다.", () => {
     render(<Button data-testid="btn">Button</Button>);
     const button = screen.getByTestId("btn");
 
     expect(button).toBeInTheDocument();
   });
 
-  it("fires an event on onClick", () => {
+  it("onClick 이벤트가 정상적으로 트리거된다.", () => {
     const fn = jest.fn();
     render(
       <Button onClick={fn} data-testid="btn">
@@ -25,5 +25,64 @@ describe("Button", () => {
     const button = screen.getByTestId("btn");
     fireEvent.click(button);
     expect(fn).toHaveBeenCalled();
+  });
+
+  test("icons와 함께 렌더링되어야 한다.", () => {
+    render(
+      <>
+        <Button leftIcon={<IoMdClose />}>Left Icon</Button>
+        <Button rightIcon={<IoMdClose />}>Right Icon</Button>
+      </>,
+    );
+    expect(screen.getByText("Left Icon")).toBeTruthy();
+    expect(screen.getByText("Right Icon")).toBeTruthy();
+  });
+
+  test("loadingText가 렌더링된다.", () => {
+    render(
+      <Button isLoading loadingText="loading...">
+        Spinner
+      </Button>,
+    );
+    expect(screen.getByText("loading...")).toBeDisabled();
+  });
+
+  test("Spinner가 렌더링된다.", () => {
+    render(
+      <Button isLoading data-testId="loading">
+        Spinner
+      </Button>,
+    );
+    expect(screen.getByTestId("loading")).toBeDisabled();
+  });
+
+  test("비활성화 된다.", () => {
+    render(<Button isDisabled>Disabled</Button>);
+    expect(screen.getByText("Disabled")).toBeDisabled();
+  });
+
+  test("aria 속성이 정상적으로 적용된다.", () => {
+    render(<Button>Aria Button</Button>);
+    expect(screen.getByText("Aria Button")).not.toHaveAttribute(
+      "aria-disabled",
+    );
+
+    render(
+      <Button isLoading data-testId="loading-button">
+        Loading Button
+      </Button>,
+    );
+    expect(screen.getByTestId("loading-button")).toHaveAttribute(
+      "aria-disabled",
+    );
+
+    render(
+      <Button isDisabled data-testId="disabled-button">
+        Disabled Button
+      </Button>,
+    );
+    expect(screen.getByTestId("disabled-button")).toHaveAttribute(
+      "aria-disabled",
+    );
   });
 });
