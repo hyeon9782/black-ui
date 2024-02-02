a712e76532a7859569b4bfa7854a93bfd90191e6
hyeon9782 <hyeon9782@gmail.com>
2024-02-03 08:04:20 +0900

    ✅ test: Tag 컴포넌트 테스트 코드 작성

--- src/components/DataDisplay/Tag/Tag.tsx
+++ src/components/DataDisplay/Tag/Tag.tsx
@@ -4,8 +4,12 @@ import { TagVariants, tag } from "./Tag.css";
 type TagProps = TagVariants & {
   children: ReactNode;
 };
-const Tag = ({ children, color, variant }: TagProps) => {
-  return <div className={tag({ color, variant })}>{children}</div>;
+const Tag = ({ children, color, variant, ...props }: TagProps) => {
+  return (
+    <div className={tag({ color, variant })} {...props}>
+      {children}
+    </div>
+  );
 };
 
 export default Tag;
