0ab3ba3f436601ee0eb73ef105c81248d3650c5c
hyeon9782 <hyeon9782@gmail.com>
2024-02-08 10:58:29 +0900

    📝 docs: Tabs 컴포넌트 Storybook 코드 수정

--- src/components/Disclosure/Tabs/Tabs.stories.tsx
+++ src/components/Disclosure/Tabs/Tabs.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Tabs from "./Tabs";
+import Tabs, { TabsProps } from "./Tabs";
 import Tab from "./Tab";
 import TabList from "./TabList";
 import TabPanel from "./TabPanel";
@@ -9,27 +9,44 @@ const meta = {
   title: "Component/Disclosure/Tabs",
   component: Tabs,
   tags: ["autodocs"],
-  parameters: { layout: "centered" },
 } satisfies Meta<typeof Tabs>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Basic: Story = {
-  args: {
-    children: (
-      <Tabs>
-        <TabList>
-          <Tab>1번 Tab</Tab>
-          <Tab>2번 Tab</Tab>
-          <Tab>3번 Tab</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>1번 내용</TabPanel>
-          <TabPanel>2번 내용</TabPanel>
-          <TabPanel>3번 내용</TabPanel>
-        </TabPanels>
-      </Tabs>
-    ),
-  },
+const Component = (args: TabsProps) => {
+  return (
+    <Tabs {...args}>
+      <TabList>
+        <Tab>1번 Tab</Tab>
+        <Tab>2번 Tab</Tab>
+        <Tab>3번 Tab</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>1번 내용</TabPanel>
+        <TabPanel>2번 내용</TabPanel>
+        <TabPanel>3번 내용</TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+};
+
+export const Line: Story = {
+  args: { size: "md", align: "start", variant: "line" },
+  render: Component,
+};
+
+export const Enclosed: Story = {
+  args: { size: "md", align: "start", variant: "enclosed" },
+  render: Component,
+};
+
+export const SoftRounded: Story = {
+  args: { size: "md", align: "start", variant: "soft-rounded" },
+  render: Component,
+};
+
+export const SolidRounded: Story = {
+  args: { size: "md", align: "start", variant: "solid-rounded" },
+  render: Component,
 };
