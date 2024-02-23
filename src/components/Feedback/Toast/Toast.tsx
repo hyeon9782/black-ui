8051d6eb5624c1ff7d8b40918688aa7b2989986b
hyeon9782 <hyeon9782@gmail.com>
2024-02-23 19:09:11 +0900

    ✨ feat: Toast 컴포넌트 추가 기능 구현
    1. status 수정
    2. icon 추가
    3. progress 추가
    4. close 추가

--- src/components/Feedback/Toast/Toast.tsx
+++ src/components/Feedback/Toast/Toast.tsx
@@ -1,6 +1,18 @@
 import { useContext, useEffect } from "react";
-import { ToastVariants, toast } from "./Toast.css";
+import {
+  ToastVariants,
+  descriptionBox,
+  icon,
+  progress,
+  time,
+  titleBox,
+  toast,
+} from "./Toast.css";
 import { ToastDispatchContext } from "./ToastProvider";
+import { CiCircleCheck, CiCircleInfo, CiWarning } from "react-icons/ci";
+import { MdOutlineDangerous } from "react-icons/md";
+import { assignInlineVars } from "@vanilla-extract/dynamic";
+import { CloseButton } from "@/components/Other";
 export type Toast = ToastVariants & {
   id?: string;
   title: string;
@@ -10,12 +22,19 @@ export type Toast = ToastVariants & {
 };
 const Toast = ({
   title,
-  duration = 3000,
+  duration = 300000,
   description,
   status,
   id = "",
 }: Toast) => {
   const { close } = useContext(ToastDispatchContext);
+
+  const getIcon = () => {
+    if (status === "success") return <CiCircleCheck />;
+    if (status === "warning") return <CiWarning />;
+    if (status === "danger") return <MdOutlineDangerous />;
+    if (status === "info") return <CiCircleInfo />;
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -26,10 +45,23 @@ const Toast = ({
       clearTimeout(timer);
     };
   }, [duration]);
+
   return (
     <div className={toast({ status })}>
-      <div>{title}</div>
-      <div>{description}</div>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <div className={icon}>{getIcon()}</div>
+        <div>
+          <div className={titleBox}>{title}</div>
+          <div className={descriptionBox}>{description}</div>
+        </div>
+        <CloseButton onClose={() => close(id)} />
+      </div>
+      <div
+        className={progress}
+        style={assignInlineVars({
+          [time]: `${duration / 1000}s`,
+        })}
+      ></div>
     </div>
   );
 };
--- src/components/Feedback/Toast/ToastList.tsx
+++ src/components/Feedback/Toast/ToastList.tsx
@@ -2,16 +2,19 @@ import { useContext } from "react";
 import { ToastStateContext } from "./ToastProvider";
 import Toast from "./Toast";
 import { wrap } from "./Toast.css";
+import { Portal } from "@/components/Other";
 
 const ToastList = () => {
   const toastList = useContext(ToastStateContext);
 
   return (
-    <div className={wrap({})}>
-      {toastList.map((toast) => {
-        return <Toast key={toast.id} {...toast} id={toast.id} />;
-      })}
-    </div>
+    <Portal>
+      <div className={wrap({})}>
+        {toastList.map((toast) => {
+          return <Toast key={toast.id} {...toast} id={toast.id} />;
+        })}
+      </div>
+    </Portal>
   );
 };
 
