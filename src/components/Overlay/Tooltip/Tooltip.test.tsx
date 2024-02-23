48a0a77dbf67627a4bf0e6919cbb249d38c8a9ae
hyeon9782 <hyeon9782@gmail.com>
2024-02-23 16:07:05 +0900

    🐛 fix: Tooltip 컴포넌트 test & storybook 코드 수정

--- src/components/Overlay/Tooltip/Tooltip.test.tsx
+++ src/components/Overlay/Tooltip/Tooltip.test.tsx
@@ -1,14 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import Tooltip from "./Tooltip";
 import { Button } from "@/components/Form";
+import TooltipTrigger from "./TooltipTrigger";
+import TooltipContent from "./TooltipContent";
 
 describe("Tooltip 컴포넌트 테스트", () => {
-  test("Tooltip 컴포넌트가 렌더링된다.", () => {
-    render(
-      <Tooltip label="Hover me">
-        <Button>Tooltip 나와라!</Button>
-      </Tooltip>,
+  const Component = (args: any) => {
+    return (
+      <Tooltip {...args}>
+        <TooltipTrigger>
+          <Button>Tooltip</Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Tooltip 입니다.</p>
+        </TooltipContent>
+      </Tooltip>
     );
+  };
+
+  test("Tooltip 컴포넌트가 렌더링된다.", () => {
+    render(<Component />);
 
     expect(screen.getByText("Tooltip 나와라!")).toBeInTheDocument();
   });
